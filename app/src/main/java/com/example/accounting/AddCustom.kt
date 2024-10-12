2e43ca59c94377b443b2a04d9ac5b382b96b42e1
atkmehmet <durmazmehmet47@gmail.com>
2024-10-13 00:17:55 +0300

    New Screen  Add

---
+++ app/src/main/java/com/example/accounting/AddCustom.kt
@@ -0,0 +1,11 @@
+package com.example.accounting
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AddCustom(){
+    Column {
+
+    }
+}
