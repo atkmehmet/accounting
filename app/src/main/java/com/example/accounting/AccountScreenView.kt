0331ce46c8c2b8fc72138e6052202c123e689346
atkmehmet <durmazmehmet47@gmail.com>
2024-10-16 00:29:52 +0300

    Add List of custom

--- app/src/main/java/com/example/accounting/AccountScreenView.kt
+++ app/src/main/java/com/example/accounting/AccountScreenView.kt
@@ -1,14 +1,20 @@
 package com.example.accounting
 
+import androidx.compose.runtime.MutableFloatState
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 
 class AccountScreenView:ViewModel() {
  private   var _state by mutableStateOf(Edits())
+ private   var list = MutableStateFlow(emptyList<Edits>())
+    val listItems = list.asStateFlow()
+
    val state :Edits
        get () =_state
 
@@ -18,26 +24,24 @@ class AccountScreenView:ViewModel() {
             name = newName
         )
     }
-
-
     fun updateSurName(surName:String){
         _state = _state.copy(
-            name = surName
+            surName = surName
         )
     }
-
     fun updateAdress(adress:String){
         _state = _state.copy(
-            name = adress
+            address = adress
         )
     }
-
-
-
     fun updateBirthDate(date:String){
         _state = _state.copy(
             birthDate = date
         )
+    }
+
+    fun addList(edt:Edits){
+        list.value += edt
     }
 
 
